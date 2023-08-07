7dd62a2f924a95d43558549d574e451207386067
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-08-07 09:52:59 -0500

    Update StackQueuePsuedo.cs

--- c-sharp/CodeChallenge11/StackQueuePsuedo.cs
+++ c-sharp/CodeChallenge11/StackQueuePsuedo.cs
@@ -1,8 +1,31 @@
-using Xunit;
 namespace CodeChallenges
 {
-  public class StackQueuePsuedo
-  {
+    public class StackQueuePsuedo
+    {
+        private Stack<int> enqueueStack = new Stack<int>();
+        private Stack<int> dequeueStack = new Stack<int>();
 
-  }
+        public void Enqueue(int item)
+        {
+            enqueueStack.Push(item);
+        }
+
+        public int Dequeue()
+        {
+            if (dequeueStack.Count == 0)
+            {
+                if (enqueueStack.Count == 0)
+                {
+                    throw new InvalidOperationException("Queue is empty.");
+                }
+
+                while (enqueueStack.Count > 0)
+                {
+                    dequeueStack.Push(enqueueStack.Pop());
+                }
+            }
+
+            return dequeueStack.Pop();
+        }
+    }
 }
